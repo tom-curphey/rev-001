6dbdd6846a63b484cc9610c36c92d6262450e704
muppet-training <tomcurphey12@gmail.com>
2019-06-03 07:58:58 +1000

    express tested

---
+++ index.js
@@ -0,0 +1,11 @@
+// This is the serve.js page
+// Add all the serve configurations here
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running...'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}...`)
+);
