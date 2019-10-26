67cf386299b0a7570f441720d472b78cebec151c
Tom Curphey <tomcurphey12@gmail.com>
2019-10-26 13:18:39 +1100

    Update console.log line
    Lets give fancier details

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}...`)
+  console.log(`Server started on your port ${PORT}...`)
 );
